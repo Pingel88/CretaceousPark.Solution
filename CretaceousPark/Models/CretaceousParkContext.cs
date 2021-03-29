2ab50c5f697e53bd1a44b31843d0541432537cb4
Michael Pingel <mdpingel@gmail.com>
2021-03-29 14:23:01 -0700

    end of lesson 7

--- CretaceousPark/Models/CretaceousParkContext.cs
+++ CretaceousPark/Models/CretaceousParkContext.cs
@@ -10,5 +10,17 @@ namespace CretaceousPark.Models
     }
 
     public DbSet<Animal> Animals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Name = "Matilda", Species = "Woolly Mammoth", Age = 7, Gender = "Female" },
+          new Animal { AnimalId = 2, Name = "Rexie", Species = "Stegosaurus", Age = 10, Gender = "Female" },
+          new Animal { AnimalId = 3, Name = "Saffron", Species = "Pachycephalosaurus", Age = 2, Gender = "Female" },
+          new Animal { AnimalId = 4, Name = "Pip", Species = "Megalodon", Age = 4, Gender = "Male" },
+          new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Therizinosaurus", Age = 22, Gender = "Male" }
+        );
+    }
   }
 }
